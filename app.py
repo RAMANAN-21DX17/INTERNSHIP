f1efc65d2d3b509d091346d34ad40e86107ebb00
RAMANAN <karmahee@gmail.com>
2023-05-30 20:10:45 +0530

    login and sign up backend

--- app.py
+++ app.py
@@ -40,8 +40,10 @@ def login():
             session['id'] = account['id']
             session['username'] = account['username']
             msg = 'Logged in successfully !'
-            print("logged in")
-            return render_template('E-HOMEPAGE.html', msg = msg)
+            if account["positon"] == "Employee":
+                return render_template('E-HOMEPAGE.html', msg = msg)
+            else:
+                return render_template('A-HOMEPAGE.html',msg =msg)
         else:
             msg = 'Incorrect username / password !'
     return render_template('LOGIN.html', msg = msg)
@@ -56,25 +58,33 @@ def logout():
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+    if request.method == 'POST':
+        usertype = request.form['usertype']
         username = request.form['username']
-        password = request.form['password']
+        company = request.form['company']
+        location = request.form['location']
         email = request.form['email']
+        password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
         account = cursor.fetchone()
+        cursor.execute('SELECT * FROM accounts WHERE company = % s', (company, ))
+        com = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
+        elif not username or not password or not email or not location or not company:
             msg = 'Please fill out the form !'
+        elif usertype == 'Admin' and com:
+            msg = 'Your Company is already registered!!'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s, % s, % s)', (username, password, email, location, company, usertype, ))
             mysql.connection.commit()
             msg = 'You have successfully registered !'
+            return render_template('LOGIN.html', msg = msg)
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('SIGNUP.html', msg = msg)
