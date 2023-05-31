ee918f89640d8cad9eed63598c6e05bb90213a26
RAMANAN <karmahee@gmail.com>
2023-05-31 12:33:08 +0530

    9

--- db.sql
+++ db.sql
@@ -6,10 +6,12 @@ username varchar(50) not null,
 password varchar(255) not null,
 email varchar(255) not null,
 location varchar(100) not null,
-company varchar(100) not null,
+company varchar(100) not null ,
 positon varchar(50) not null,
 primary key (id)
 ) engine = InnoDB auto_increment = 2 default charset = utf8;
 SELECT * FROM ACCOUNTS;
 drop table accounts;
 insert into accounts values ( 3, "employee" , "12345678" ,"shutlight7@gmail.com" , "coimbatore" , "psg", "Employee");
+	
+	
