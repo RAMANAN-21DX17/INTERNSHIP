d5a405c0e8656d6b2536c812891524d11b4af653
Barath KN <sanjaybarath69@gmail.com>
2023-06-01 08:43:11 +0530

    9

--- db.sql
+++ db.sql
@@ -14,4 +14,19 @@ SELECT * FROM ACCOUNTS;
 drop table accounts;
 insert into accounts values ( 3, "employee" , "12345678" ,"shutlight7@gmail.com" , "coimbatore" , "psg", "Employee");
 	
+create table bookings(
+company varchar(50) not null ,
+room varchar(100) not null ,
+timing varchar(50) not null ,
+available varchar(20) not null ,
+primary key (id)
+)
+SELECT * FROM bookings;
+insert into bookings values('p','q','9-10','yes');
+insert into bookings values('p','q','10-11','yes');
+insert into bookings values('p','q','11-12','N0');
+insert into bookings values('p','q','12-1','yes');
+insert into bookings values('p','q','1-2','No');
+insert into bookings values('p','q','2-3','yes');
+insert into bookings values('p','q','3-4','yes');    
 	
